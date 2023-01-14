0baf7efaaaae690993d4696278277ac9c4e92d9f
Vladimir Bucur <vladimirbucur1@gmail.com>
2023-01-14 17:40:11 +0200

    add fileio classes

---
+++ proiect2/src/fileio/ContainsInput.java
@@ -0,0 +1,42 @@
+package fileio;
+
+import java.util.ArrayList;
+
+public final class ContainsInput {
+    private ArrayList<String> actors;
+    private ArrayList<String> genre;
+
+    public ContainsInput() { }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<String> getActors() {
+        return actors;
+    }
+
+    /**
+     *
+     * @param actors
+     */
+    public void setActors(final ArrayList<String> actors) {
+        this.actors = actors;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<String> getGenres() {
+        return genre;
+    }
+
+    /**
+     *
+     * @param genre
+     */
+    public void setGenre(final ArrayList<String> genre) {
+        this.genre = genre;
+    }
+}
